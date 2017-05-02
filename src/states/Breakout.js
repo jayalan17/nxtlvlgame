f5733c261c9d66bc2811625686fedcf6539f0fd6
Jeffrey Alan Pawlak <jeffrey_pawlak@yahoo.com>
2017-05-02 16:57:52 -0600

    set all levels to full

--- src/states/Breakout.js
+++ src/states/Breakout.js
@@ -75,7 +75,7 @@ export default class Breakout extends Phaser.State {
       this.dieSound.play();
       this.score = 0;
     }
-    if (this.score === 100) {
+    if (this.score === 300) {
       window.game.breakoutHighScore = this.score;
       this.state.start('BreakoutWin');
       window.game.breakoutScoreUpdate();
